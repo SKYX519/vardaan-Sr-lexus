74acb6028dc97a05a944441b44f159e14e288001
SAHIL_K0S <137023745+SKYX519@users.noreply.github.com>
2026-09-10 20:58:04 +0530

    Time: 31 ms (66.68%) | Memory: 13 MB (90.87%) - LeetSync

---
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
@@ -0,0 +1,44 @@
+class Solution:
+    def findSubstring(self, s, words):
+        from collections import Counter
+
+        word_len = len(words[0])
+        word_count = len(words)
+        total_len = word_len * word_count
+
+        target = Counter(words)
+        ans = []
+
+        for start in range(word_len):
+            left = start
+            right = start
+            count = 0
+            seen = {}
+
+            while right + word_len <= len(s):
+                word = s[right:right + word_len]
+                right += word_len
+
+                if word in target:
+                    seen[word] = seen.get(word, 0) + 1
+                    count += 1
+
+                    while seen[word] > target[word]:
+                        left_word = s[left:left + word_len]
+                        seen[left_word] -= 1
+                        left += word_len
+                        count -= 1
+
+                    if count == word_count:
+                        ans.append(left)
+
+                        left_word = s[left:left + word_len]
+                        seen[left_word] -= 1
+                        left += word_len
+                        count -= 1
+                else:
+                    seen.clear()
+                    count = 0
+                    left = right
+
+        return ans
